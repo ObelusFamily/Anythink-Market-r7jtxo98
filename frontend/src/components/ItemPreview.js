bd4bc837bdca430db411df76c3cb0969c0b35067
Steven Armoo <armoosteven1@gmail.com>
2023-01-10 18:41:45 +0000

    CHORE show in the UI, if sellers are verified or not

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -54,8 +54,13 @@ const ItemPreview = (props) => {
               className="user-pic rounded-circle pr-1"
             />
           </Link>
-          <img src={process.env.PUBLIC_URL + "verified_seller.svg"} alt="verified icon" />
-          <p>TOP SELLER</p>
+          {
+            item.seller.isVerified &&
+            <>
+              <img src={process.env.PUBLIC_URL + "verified_seller.svg"} alt="verified icon" />
+              <p>TOP SELLER</p>
+            </>
+          }
           <button className="btn btn-outline-secondary" onClick={handleClick}>
             <i className="ion-heart"></i> {item.favoritesCount}
           </button>
